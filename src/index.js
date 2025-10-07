ff9dce4191acfa25c2e28f19b70cb26e7c63619b
Yoven Reddi <yovenre1@gmail.com>
2025-10-07 06:00:45 -0400

    Updated landing page files

--- src/index.js
+++ src/index.js
@@ -1,24 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Home from "./Home";
-import Features from "./Features";
-import Industries from "./Industries";
-import Contact from "./Contact";
-import About from "./About";
+import "./index.css";
+import ReneAILanding from "./Home";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/features" element={<Features />} />
-        <Route path="/industries" element={<Industries />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </BrowserRouter>
+    <ReneAILanding />
   </React.StrictMode>
 );
